6844b49fb5b05c7081981d54407d9ba9672752c2
ninjarob <robatmywork@gmail.com>
2017-08-28 21:16:37 -0600

    First Commit Also

---
+++ src/main/java/com/koolkevans/firmfoundations/services/UserController.java
@@ -0,0 +1,21 @@
+package com.koolkevans.firmfoundations.services;
+
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/students/{studentId}/courses")
+    public List<Course> retrieveCoursesForStudent(@PathVariable String studentId) {
+        return studentService.retrieveCourses(studentId);
+    }
+
+    @GetMapping("/students/{studentId}/courses/{courseId}")
+    public Course retrieveDetailsForCourse(@PathVariable String studentId,
+                                           @PathVariable String courseId) {
+        return studentService.retrieveCourse(studentId, courseId);
+    }
+
+}
