54d940cb6e9879a30f9f6e1c54f1b183d00b9d63
ninjarob <robatmywork@gmail.com>
2017-08-28 21:44:27 -0600

    First Commit Also

--- src/main/java/com/koolkevans/firmfoundations/controllers/UserController.java
+++ src/main/java/com/koolkevans/firmfoundations/controllers/UserController.java
@@ -1,21 +1,21 @@
 package com.koolkevans.firmfoundations.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    @Autowired
-    private StudentService studentService;
-
     @GetMapping("/students/{studentId}/courses")
-    public List<Course> retrieveCoursesForStudent(@PathVariable String studentId) {
-        return studentService.retrieveCourses(studentId);
+    public void retrieveCoursesForStudent(@PathVariable String studentId) {
+        //return studentService.retrieveCourses(studentId);
     }
 
     @GetMapping("/students/{studentId}/courses/{courseId}")
-    public Course retrieveDetailsForCourse(@PathVariable String studentId,
+    public void retrieveDetailsForCourse(@PathVariable String studentId,
                                            @PathVariable String courseId) {
-        return studentService.retrieveCourse(studentId, courseId);
+        //return studentService.retrieveCourse(studentId, courseId);
     }
 
 }
